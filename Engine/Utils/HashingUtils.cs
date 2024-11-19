c991f617535e1bad4b7b140b1244cf59170b3edd
Simon Konieczny <konieczny.szymon@icloud.com>
2024-11-19 23:34:21 +0000

    Created base project and added first hashing utils

---
+++ Engine/Utils/HashingUtils.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+namespace Engine.Utils;
+
+public class HashingUtils
+{
+    public static byte[] ComputeHash(string input, string hashType)
+    {
+        using HashAlgorithm hashingAlgorithm = hashType.ToUpper() switch
+        {
+            "MD5" => MD5.Create(),
+            "SHA-1" => SHA1.Create(),
+            "SHA-256" => SHA256.Create(),
+            _ => throw new ArgumentException("Unsupported hashing type", hashType)
+        };
+        return hashingAlgorithm.ComputeHash(System.Text.Encoding.UTF8.GetBytes(input));
+    }
+    
+    public static string ButeArrayToHexString(byte[] bytes)
+    => BitConverter.ToString(bytes).Replace("-", "").ToLowerInvariant();
+}
